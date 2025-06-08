29fbce020ddc61161c6ab3a9694492fabbd930aa
DEV MASTER <53788141+STIFLEUR390@users.noreply.github.com>
2025-06-08 08:19:54 +0100

    update selenuim

--- selenium/entrypoint.sh
+++ selenium/entrypoint.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-echo "[ENTRYPOINT] Chargement des proxys :"
+echo "[INFO] Téléchargement de la liste des proxys..."
+
+curl -s "https://proxylist.geonode.com/api/proxy-list?limit=500&page=1&sort_by=lastChecked&sort_type=desc" \
+ | jq -r '.data[] | select(.protocols[] | test("http")) | "\(.ip):\(.port)"' \
+ | head -n 50 \
+ | paste -sd "," - \
+ > /opt/proxies.txt
+
+echo "[INFO] Proxys enregistrés dans /opt/proxies.txt"
 cat /opt/proxies.txt
 
-# Lancement de Selenium
+echo "[INFO] Lancement de Selenium"
 exec /opt/bin/entry_point.sh
